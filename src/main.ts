580474e104fa58d0246f968117d31a05010d7ac4
koenschuite <tse.lao@yahoo.com>
2023-10-05 15:08:35 +0200

    api settings

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import * as process from 'process';
 import { AppModule } from './app.module';
@@ -11,6 +12,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  
+  const config = new DocumentBuilder()
+  .setTitle('Careerzen API')
+  .setDescription('Careerzen backend solutions for getting peoples profile, companies and different builds. ')
+  .setVersion('1.0')
+  .addTag('bu')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 
